d54b38938746c06d2a4235ec82524ad0a836899a
Pavlo1508 <130464415+Pavlo1508@users.noreply.github.com>
2024-02-12 19:03:59 +0000

    Deploying to gh-pages from @ Pavlo1508/goit-js-hw-10@db173dfa07d89341d8c8427093fe6ba0a2a86c3d 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,5 +1,5 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-77e16229.js";const e=document.querySelector(".form");function t(){return`
-  <label>
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";const t=document.querySelector(".form");function m(){return`
+  <label class="delay-label">
     Delay (ms)
     <input type="number" name="delay" required />
   </label>
@@ -16,5 +16,5 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-77e16
     </label>
   </fieldset>
 
-  <button type="submit">Create notification</button>`}e.innerHTML=t();
+  <button type="submit">Create notification</button>`}t.innerHTML=m();const c=document.querySelector("label > input");let o;c.addEventListener("input",l=>{o=l.currentTarget.value});t.addEventListener("submit",l=>{l.preventDefault();const r=t.elements.state.value;function n(e,a){return new Promise((s,u)=>{setTimeout(()=>{a==="fulfilled"?s(e):u(e)},e)})}n(o,r).then(e=>{i.show({iconUrl:"../img/bi_check2-circle.png",title:"OK",titleColor:"#fff",message:`Fulfilled promise in ${e} ms`,messageColor:"#FFFFFF",backgroundColor:"#59A10D",position:"topRight"})}).catch(e=>{i.show({iconUrl:"../img/bi_x-octagon.png",title:"Error",titleColor:"#fff",message:`Rejected promise in ${e} ms`,messageColor:"#FFFFFF",backgroundColor:"#EF4040",position:"topRight"})}),t.reset()});
 //# sourceMappingURL=commonHelpers2.js.map
