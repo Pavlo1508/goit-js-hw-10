02d8e232101f14fff1cba186fef8bfc843f2bca2
Pavlo1508 <130464415+Pavlo1508@users.noreply.github.com>
2024-02-12 17:19:26 +0000

    Deploying to gh-pages from @ Pavlo1508/goit-js-hw-10@922ace7e23931349f7f664e11ec78f7c8a1545a9 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,20 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-77e16229.js";const e=document.querySelector(".form");function t(){return`
+  <label>
+    Delay (ms)
+    <input type="number" name="delay" required />
+  </label>
+
+  <fieldset>
+    <legend>State</legend>
+    <label>
+      <input type="radio" name="state" value="fulfilled" required />
+      Fulfilled
+    </label>
+    <label>
+      <input type="radio" name="state" value="rejected" required />
+      Rejected
+    </label>
+  </fieldset>
+
+  <button type="submit">Create notification</button>`}e.innerHTML=t();
+//# sourceMappingURL=commonHelpers2.js.map
