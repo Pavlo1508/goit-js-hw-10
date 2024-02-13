acc8d61d869294f09fbcd4e20e0b02ef14fbf8f4
Pavlo1508 <130464415+Pavlo1508@users.noreply.github.com>
2024-02-13 18:46:18 +0000

    Deploying to gh-pages from @ Pavlo1508/goit-js-hw-10@8afe36976e58e4f8770aba839e1e72bd0a3ab8c2 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{i as o}from"./assets/vendor-77e16229.js";const m="../img/bi_check2-circle.png",c="../img/bi_x-octagon.png",t=document.querySelector(".form");function d(){return`
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i as l}from"./assets/vendor-77e16229.js";const m="../img/bi_check2-circle.png",t=document.querySelector(".form");function c(){return`
   <label class="delay-label">
     Delay (ms)
     <input type="number" name="delay" required />
@@ -16,5 +16,5 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{i as o}from"./assets/
     </label>
   </fieldset>
 
-  <button type="submit">Create notification</button>`}t.innerHTML=d();const f=document.querySelector("label > input");let l;f.addEventListener("input",i=>{l=i.currentTarget.value});t.addEventListener("submit",i=>{i.preventDefault();const r=t.elements.state.value;function n(e,a){return new Promise((s,u)=>{setTimeout(()=>{a==="fulfilled"?s(e):u(e)},e)})}n(l,r).then(e=>{o.show({image:m,title:"OK",titleColor:"#fff",message:`Fulfilled promise in ${e} ms`,messageColor:"#FFFFFF",backgroundColor:"#59A10D",position:"topRight"})}).catch(e=>{o.show({image:c,title:"Error",titleColor:"#fff",message:`Rejected promise in ${e} ms`,messageColor:"#FFFFFF",backgroundColor:"#EF4040",position:"topRight"})}),t.reset()});
+  <button type="submit">Create notification</button>`}t.innerHTML=c();const d=document.querySelector("label > input");let o;d.addEventListener("input",i=>{o=i.currentTarget.value});t.addEventListener("submit",i=>{i.preventDefault();const r=t.elements.state.value;function n(e,a){return new Promise((s,u)=>{setTimeout(()=>{a==="fulfilled"?s(e):u(e)},e)})}n(o,r).then(e=>{l.show({image:m,title:"OK",titleColor:"#fff",message:`Fulfilled promise in ${e} ms`,messageColor:"#FFFFFF",backgroundColor:"#59A10D",position:"topRight"})}).catch(e=>{l.show({image:"../img/bi_check2-circle.png",imageWidth:50,title:"Error",titleColor:"#fff",message:`Rejected promise in ${e} ms`,messageColor:"#FFFFFF",backgroundColor:"#EF4040",position:"topRight"})}),t.reset()});
 //# sourceMappingURL=commonHelpers2.js.map
